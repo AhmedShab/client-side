5c849600fbaf8a4d39c069d49bb7c9d11f74881a
AhmedShab <ahmed.vuw@gmail.com>
2016-08-09 13:31:27 +1200

    getting data from the api

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var xhr = require('xhr')
-var greeting = require('./views/greeting.hbs')
+var xhr = require('xhr');
+var greeting = require('./views/greeting.hbs');
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites';
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
-    console.error(err)
+    console.error(err);
   }
 
+  var result = JSON.parse(data.body);
+
   // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+  // console.log(result[0].name); // FYI: data.body is a string
 
   // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
-})
+  var target = document.getElementsByTagName('main')[0];
+  target.innerHTML = greeting({name: result[0].name});
+});
+
+function getData() {
+  console.log('get data');
+}
--- tests/greeting.js
+++ tests/greeting.js
@@ -1,21 +1,19 @@
-var test = require('tape')
-var Handlebars = require('handlebars')
-var fs = require('fs')
-var path = require('path')
-var cheerio = require('cheerio')
+var test = require('tape');
+var Handlebars = require('handlebars');
+var fs = require('fs');
+var path = require('path');
+var cheerio = require('cheerio');
 
 test('greeting view', function(t) {
-  var templateText = fs.readFileSync(path.join(__dirname, 'views/greeting.hbs'), 'utf8')
-  var templateFunction = Handlebars.compile( templateText )
+  var templateText = fs.readFileSync(path.join(__dirname, 'views/greeting.hbs'), 'utf8');
+  var templateFunction = Handlebars.compile( templateText );
 
-  var exampleData = {name: 'MIXMIX'}
+  var exampleData = {name: 'MIXMIX'};
 
-  var renderedTemplate = templateFunction(exampleData)
-  var $ = cheerio.load(renderedTemplate)
+  var renderedTemplate = templateFunction(exampleData);
+  var $ = cheerio.load(renderedTemplate);
 
-  t.equal($('h1').text(), "Hello MIXMIX!", 'template uses data in heading')
+  t.equal($('h1').text(), "Hello MIXMIX!", 'template uses data in heading');
 
-  t.end()
-})
-
-
+  t.end();
+});
